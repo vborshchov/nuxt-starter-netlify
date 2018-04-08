06cabad0c6392f1d1dd7e487b99d7ee59914cf60
vborshchov <victor.borshchov@gmail.com>
2018-04-08 14:39:38 +0300

    add files

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <article>
+    <h1>{{ title }}</h1>
+    <p>{{body}}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    // const postPromise = process.BROWSER_BUILD
+    //   ? import('~/content/blog/posts/' + params.slug + '.json')
+    //   : Promise.resolve(
+    //       require('~/content/blog/posts/' + params.slug + '.json')
+    //     );
+
+    let post = await import('~/content/blog/posts/' + params.slug + '.json');
+    return post;
+  }
+};
+</script>
