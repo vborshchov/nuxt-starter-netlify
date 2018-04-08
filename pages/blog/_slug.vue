1f0a4a59778d1f41a422dc6ce27f785bdf560f53
vborshchov <victor.borshchov@gmail.com>
2018-04-08 15:45:31 +0300

    add vue markdown

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,12 +1,17 @@
 <template>
   <article>
     <h1>{{ title }}</h1>
-    <p>{{body}}</p>
+    <vue-markdown>{{body}}</vue-markdown>
   </article>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+
 export default {
+  components: {
+    VueMarkdown
+  },
   async asyncData({ params }) {
     // const postPromise = process.BROWSER_BUILD
     //   ? import('~/content/blog/posts/' + params.slug + '.json')
